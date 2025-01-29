0e455020970e40ff3061ff8fac2b7b9eafd933ab
Leonardo Almeida <almeidaleo.dev@gmail.com>
2025-01-29 15:26:15 -0300

    feat(data): create and configure DbContext with DbSets
    Define the application's DbContext and establish DbSets for entity management.
    
    Ensure proper configuration for database interaction and entity tracking.

---
+++ EventosPro/Context/ApplicationDbContex.cs
@@ -0,0 +1,14 @@
+﻿using EventosPro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventosPro.Context
+{
+    public class ApplicationDbContex : DbContext 
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventInvite> Invites { get; set; }
+    }
+}
