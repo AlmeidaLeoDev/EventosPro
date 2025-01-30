5408cbbbd500f31ae3152a3441f9f8de055cdaf8
Leonardo Almeida <almeidaleo.dev@gmail.com>
2025-01-30 17:13:49 -0300

    feat(dependency injection): register repository implementations in DI container
    Register all repository implementations in the dependency injection container within Program.cs.

--- EventosPro/Program.cs
+++ EventosPro/Program.cs
@@ -1,4 +1,6 @@
 using EventosPro.Context;
+using EventosPro.Repositories.Implementations;
+using EventosPro.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContex>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventInviteRepository, EventInviteRepository>();
 
 var app = builder.Build();
 
