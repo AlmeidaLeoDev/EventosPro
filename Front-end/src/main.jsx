2f04f309b57aecdce5b83977e822ac3a9de3396d
Leonardo Almeida <almeidaleo.dev@gmail.com>
2025-02-03 02:38:25 -0300

    chore(setup):[front-end] update entry point to main.jsx and adjust project structure
    Modifi main.jsx as the main entry point rename some files for better organization.
    
    Add the start script in package.json to ensure the server runs properly.

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/styles';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-  </StrictMode>,
-)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
